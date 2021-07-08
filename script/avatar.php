27e661dac89ea0788e480b0ce4d02c97e1462c98
Unknown <54515356+ItsDevUnknown@users.noreply.github.com>
2021-07-08 12:05:35 -0500

    Create avatar.php

---
+++ script/avatar.php
@@ -0,0 +1,46 @@
+<?php
+
+function createAvatarImage($string)
+{
+    $imageFilePath = "../assets/avatar/" . $string . ".png";
+
+    if (!file_exists($imageFilePath))
+    {
+        /*
+            Base avatar image that we use to center
+            our text string on top of it
+        */
+        $avatar = imagecreate(185,190);
+
+        /*
+            Assign random RGB values
+        */
+
+        $r = rand(50,200); /* r (red) */
+        $g = rand(50,200); /* g (green) */
+        $b = rand(50,200); /* b (blue) */
+
+        imagecolorallocate($avatar, $r, $g, $b);
+        $textcolor = imagecolorallocate($avatar, 255, 255, 255);
+        $font = 'C:\xampp\htdocs\botsym\assets\fonts\bold.ttf';
+        $font = mb_convert_encoding($font, 'big5', 'utf-8');
+
+        include "../helper/sizeavatar.php";
+
+        imagettftext($avatar, 90, 0, $x, $y, $textcolor, $font, $string);
+
+        /*
+            Header("Content-type: image/png");
+        */
+        imagepng($avatar, $imageFilePath);
+        imagedestroy($avatar);
+        return $imageFilePath;
+    } 
+    else 
+    {
+        $imageFilePath = "../assets/avatar/" . $string . ".png";
+        return $imageFilePath;
+    }
+}
+
+?>
